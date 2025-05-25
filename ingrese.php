e127b3e330447911f317c0a3f7e65d43066136cb
Jessica Triviño <jessitries@gmail.com>
2025-05-25 13:58:02 -0500

    rediseñe el loguin y el ingreo

--- ingrese.php
+++ ingrese.php
@@ -4,17 +4,35 @@
 <head>
     <title>principal | estudiante</title>
     <link rel="stylesheet" href="ingrese.css">
-    
+    <link rel="stylesheet" href="pagina principal/pagina_principal.css">
+
 </head>
 
 <body>
 
-    <div id="nav_principal">
+    <div class="nav_principal" id="nav_principal">
 
-        <div id="nav_titulo">
-        
+        <div class="nav_titulo" id="nav_titulo">
+            <img src="../imagenes/logo.jpeg" alt="logo" class="logo-nav">
             <h2>UNIVERSIDAD NUEVA Y DIFERENTE</h2>
         </div>
+
+        <button class="botonmovil" id="botonmovil">☰</button>
+
+        <ul class="nav_link" id="nav_link">
+            <li><button class = "boton1 reset-button-style" id = "id_boton1" type="button">horario</button></li>
+            <li><button class = "boton3 reset-button-style" id = "id_boton2" type="button">promedio</button></li>
+            <li><button class = "boton4 reset-button-style" id = "id_boton3" type="button">registro de materias</button></li>
+            <li><button class = "boton5 reset-button-style" id = "id_boton4" type="button">registro de horario</button></li>
+            <li><button class = "boton6 reset-button-style" id = "id_boton5" type="button">cancelacion de materias</button></li>
+            <li><button class = "boton7 reset-button-style" id = "id_boton6" type="button">notas por semestre</button></li>
+            <button class = "boton8 reset-button-style" id = "id_boton7" type="button">cerrar sesion</button></li>
+        </ul>
+        
+
+    </div>
+
+   <!--<div id="nav_principal">
 
         <div class = "borde_botones"id="nav_enlaces">
 
@@ -26,9 +44,10 @@
             <button class = "boton7" id = "id_boton6" type="button">notas por semestre</button>
             <button class = "boton8" id = "id_boton7" type="button">cerrar sesion</button>
 
-        </div>
+        </div> 
 
-    </div>    
+    </div> -->
+
     <div id="formulario1" class="form-container" style = "display: none" >    
         <form action="procesar_formulario.php" method="post">
             <label for="paises">año</label><br>
--- login.php
+++ login.php
@@ -3,38 +3,32 @@
 <head>
     <meta charset="UTF-8">
     <title>Login de Usuarios</title>
-    <link Rel = StyleSheet href="login.css" type="text/css">
+    <link rel="stylesheet" href="login.css">
 </head>
 <body>
-    <div class="container">
-        <div class="text-center caja-login" style="background-color: #aaa;">
-            <p class = "formulario_login_fuente_cabecera">universidad nueva y diferente</p>
-            <div class = formulario_login>
-                <form method="POST" action="./verificar_login.php">
 
-                    <div class="formulario_login_cabecera">
-                        <p class ="formulario_login_cabecera">Iniciar sesion</p>
-                    </div>
+    <div class="login_container">
+       
+        <h2 >INICIAR SESION</h2>
+            
+        <form method="POST" action="./verificar_login.php">
 
-                    <div class = "login_gmail">
+            <div class = "form_group">
 
-                        <tr><p class ="frase_gmail">Escribir gmail </p></tr>
-                        <input class="gmail" type="text" name="user" placeholder = "Gmail">
-                        <br><br>
-                    </div>
-
-                    <div class = "login_contraseña">
-                        <p class ="frase_contraseña">Escribir contraseña </p>
-                        <input class="contraseña" type="password" name="pass" placeholder = "Contraseña" ><br><br>
-                    </div>
-
-                    <div class = "login_boton">
-                        <button class="boton" type="submit" class = "boton-estilo"> Ingresar</button>
-                    </div>
-
-                </form>
+                <label >correo electronico </label>
+                 <input class="gmail" type="text" name="user" placeholder = "Gmail">
+                
             </div>
-        </div>
+
+            <div class = "form_group">
+                <label >contraseña </label>
+                <input class="contraseña" type="password" name="pass" placeholder = "Contraseña" ><br><br>
+            </div>
+
+                    
+            <button class="boton" type="submit" class = "boton-estilo"> INGRESAR</button>
+
+        </form>
     </div>
 </body>
 </html>
