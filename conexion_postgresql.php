b0fdb549ee9070db2ae1125b0774f19b7acd7b5c
Jessica Triviño <jessitries@gmail.com>
2025-05-25 11:01:42 -0500

    rediseñe pag principal

--- conexion_postgresql.php
+++ conexion_postgresql.php
@@ -1,12 +1,18 @@
 <?php
 $host = "localhost";
 $port = "5432";
-$dbname = "universidad_nueva_y_diferente";
+$dbname = "UND";
 $user = "postgres";
-$password = "1234";
+$password = "9707";
 
 // Cadena de conexión
 $conexion = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
 
+// Verifica si hay error
+if (!$conexion) {
+    echo "❌ Error al conectar a PostgreSQL: " . pg_last_error();
+} else {
+    echo "✅ Conexión exitosa a PostgreSQL.";
+}
 
 ?>
